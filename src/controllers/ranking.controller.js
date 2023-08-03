419a44a588ca9ee3880fdce98dca1cc309795e64
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-08-03 13:07:03 -0300

    feat: controller ranking implementado

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -1,5 +1,24 @@
 import { db } from '../database/database.js';
 
 export const getRanking = async (req, res) => {
-  res.send('getRanking');
+  try {
+    const { rows } = await db.query(`
+      SELECT users.id, users.name,
+        COALESCE(CAST(SUM(urls."visitCount") AS INTEGER), 0) AS "visitCount",
+        CAST(COUNT(urls.id) AS INTEGER) AS "linksCount"
+      FROM users
+        LEFT JOIN urls 
+        ON users.id = urls."userId"
+      GROUP BY users.id
+      ORDER BY "visitCount" DESC, "linksCount" DESC
+      LIMIT 10
+    ;`);
+    /*LEFT JOIN é necessário porque podem estar inclusos usuarios sem links 
+    encurtados, então não há colunas na tabela urls referente a eles*/
+
+    res.status(200).send(rows);
+
+  } catch ({ detail }) {
+    res.status(500).send(detail);
+  }
 }
